0f3f469bd919b42a2416c25b02e8052e3e52dec9
Łukasz <lukaszszymura9@gmail.com>
2023-02-16 10:15:30 +0100

    okok

--- panel.php
+++ panel.php
@@ -51,108 +51,157 @@
             </button>
         </div>
     </div>
-    <div class="content">
-        <div class="blur">
-            <div class="contentLeft">
-                <div class="realContent">
-                    <div class="leftTop">
-                        <div class="adminDel">
-                            <div class="adminDelHeader">
-                                <h3 style="margin: 0">Usuwanie Administratora</h3>
-                            </div>
-                            <form class="adminDelForm" method="post" action="panel.php">
-                                <select class="adminDelSelect" name="user" id="select" required>
-                                    <option value="id">okok</option>
-                                    <option value="id">okok</option>
-                                    <option value="id">okok</option>
-                                    <option value="id">okok</option>
-                                </select>
-                                <input class="delete" type="submit" value="Usuń">
-                            </form>
-                        </div>
-                        <div class="recordsClear">
-                            <div class="recordsClearHeader">
-                                <h3 style="margin: 0">Usuń Wszystkie Rekordy</h3>
-                            </div>
-                            <div class="recordsClearForm">
-                                <form action="panel.php" method="post">
+        <div class="content">
+            <div class="blur">
+                <div class="contentLeft">
+                    <div class="realContent">
+                        <div class="leftTop">
+                            <div class="adminDel">
+                                <div class="adminDelHeader">
+                                    <h3 style="margin: 0">Usuwanie Administratora</h3>
+                                </div>
+                                <form class="adminDelForm" method="post" action="panel.php">
+                                    <select class="adminDelSelect" name="user" id="select" required>
+                                        <option value="id">okok</option>
+                                        <option value="id">okok</option>
+                                        <option value="id">okok</option>
+                                        <option value="id">okok</option>
+                                    </select>
                                     <input class="delete" type="submit" value="Usuń">
                                 </form>
                             </div>
-                        </div>
-                    </div>
-                    <div class="holder">
-                        <div class="adminAdd">
-                            <div class="adminAddHeader">
-                                <h3 style="margin: 0">Dodawanie Administratora</h3>
+                            <div class="recordsClear">
+                                <div class="recordsClearHeader">
+                                    <h3 style="margin: 0">Usuń Wszystkie Rekordy</h3>
+                                </div>
+                                <div class="recordsClearForm">
+                                    <form action="panel.php" method="post">
+                                        <input class="delete" type="submit" value="Usuń">
+                                    </form>
+                                </div>
                             </div>
-                            <div class="adminAddContent">
-                                <form class="adminAddForm" action="panel.php" method="post">
-                                    <div class="adminAddInputs">
+                        </div>
+                        <div class="holder">
+                            <div class="adminAdd">
+                                <div class="adminAddHeader">
+                                    <h3 style="margin: 0">Dodawanie Administratora</h3>
+                                </div>
+                                <div class="adminAddContent">
+                                    <form class="adminAddForm" action="panel.php" method="post">
+                                        <div class="adminAddInputs">
+                                            <div class="pos">
+                                                <label for="username">Nazwa użytkownika</label>
+                                                <input class="addInput" type="text" name="username" id="username">
+                                            </div>
+                                            <div class="pos">
+                                                <label for="password">Hasło</label>
+                                                <input class="addInput" type="text" name="password" id="password">
+                                            </div>
+                                            <div class="pos">
+                                                <label for="password">Zdjęcie profilowe</label>
+                                                <input class="addInput" type="file" name="file" id="file">
+                                            </div>
+                                        </div>
+                                        <div class="adminAddButton">
+                                            <input class="add" type="submit" value="Dodaj">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="holder">
+                            <div class="addResult">
+                                <div class="addResultHeaderHolder">
+                                    <h3 class="addResultHeader">Dodawanie wyniku</h3>
+                                </div>
+                                <form action="panel.php" method="post" style="width: 100%; height: auto;">
+                                    <div class="addResultContent">
                                         <div class="pos">
                                             <label for="username">Nazwa użytkownika</label>
                                             <input class="addInput" type="text" name="username" id="username">
                                         </div>
                                         <div class="pos">
-                                            <label for="password">Hasło</label>
-                                            <input class="addInput" type="text" name="password" id="password">
+                                            <label for="score">Wynik</label>
+                                            <input class="addInput" type="text" name="score" id="score">
                                         </div>
-                                        <div class="pos">
-                                            <label for="password">Zdjęcie profilowe</label>
-                                            <input class="addInput" type="file" name="file" id="file">
+                                        <div class="pos okok">
+                                            <input class="add" type="submit" value="Dodaj">
                                         </div>
                                     </div>
-                                    <div class="adminAddButton">
-                                        <input class="add" type="submit" value="Add">
+                                </form>
+                            </div>
+                        </div>
+                        <div class="holder">
+                            <div class="addResult">
+                                <div class="addResultHeaderHolder">
+                                    <h3 class="addResultHeader">Zmiana hasła</h3>
+                                </div>
+                                <form action="panel.php" method="post" style="width: 100%; height: auto;">
+                                    <div class="addResultContent">
+                                        <div class="pos">
+                                            <label for="username">Nazwa użytkownika</label>
+                                            <select class="adminDelSelect resetPasswordSelect" name="user" id="select" required>
+                                                <option value="id">okok</option>
+                                                <option value="id">okok</option>
+                                                <option value="id">okok</option>
+                                                <option value="id">okok</option>
+                                            </select>
+                                        </div>
+                                        <div class="pos">
+                                            <label for="score">Nowe hasło</label>
+                                            <input class="addInput" type="text" name="password" id="password">
+                                        </div>
+                                        <div class="pos okok">
+                                            <input class="add" type="submit" value="Zmień">
+                                        </div>
                                     </div>
                                 </form>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="resultContent">
-                    <div class="result">
-                        <h2>Witaj w Panelu Administratora!</h2>
+                    <div class="resultContent">
+                        <div class="result">
+                            <h2>Witaj w Panelu Administratora!</h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="contentRight">
-                <div class="table">
-                    <div class="scoreboard">
-                        <table>
-                            <?php
-                            $connect = @new mysqli("localhost", "root", "", "themaze");
-                            $sql = "SELECT * FROM `score`";
-                            $result = mysqli_query($connect, $sql);
-                            if(!$connect){
-                                echo("Problem połączenia z bazą");
-                                exit();
-                            }
-                            if(!$result){
-                                echo("Problem z zapytaniem do bazy");
-                                exit();
-                            }
+                <div class="contentRight">
+                    <div class="table">
+                        <div class="scoreboard">
+                            <table>
+                                <?php
+                                $connect = @new mysqli("localhost", "root", "", "themaze");
+                                $sql = "SELECT * FROM `score`";
+                                $result = mysqli_query($connect, $sql);
+                                if(!$connect){
+                                    echo("Problem połączenia z bazą");
+                                    exit();
+                                }
+                                if(!$result){
+                                    echo("Problem z zapytaniem do bazy");
+                                    exit();
+                                }
 
-                            while($row = mysqli_fetch_assoc($result)){
-                                echo("<tr>
-                                <td>".$row["name"]."</td>
-                                <td>".$row["time"]." Czas</td>
-                                <td>
-                                    <form action='panel.php' method='post'>
-                                        <input type='hidden' name='hidden' value=".$row["id"].">
-                                        <input class='delete' type='submit' value='Usuń'>
-                                    </form>
-                                </td></tr>
-                                ");
-                            }
-                            ?>
-                        </table>
+                                while($row = mysqli_fetch_assoc($result)){
+                                    echo("<tr>
+                                    <td>".$row["name"]."</td>
+                                    <td>".$row["time"]." Czas</td>
+                                    <td>
+                                        <form action='panel.php' method='post'>
+                                            <input type='hidden' name='hidden' value=".$row["id"].">
+                                            <input class='delete' type='submit' value='Usuń'>
+                                        </form>
+                                    </td></tr>
+                                    ");
+                                }
+                                ?>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <div id="loader" class="loader">
     <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
 </div>
